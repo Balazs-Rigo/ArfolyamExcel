4994a9df01ed006195fe5958e5ec8341dd58927b
Balazs Rigo <rigo.balazs86@gmail.com>
2022-12-24 19:28:27 +0100

    Create Instances - currencies, units, exchangeRates

---
+++ KPMG.Arfolyam/KPMG.Arfolyam/Program.cs
@@ -0,0 +1,56 @@
+﻿using KPMG.Arfolyam.MNBArfolyamServiceSoapClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace KPMG.Arfolyam
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MNBArfolyamServiceSoapClient.MNBArfolyamServiceSoapClient client = new MNBArfolyamServiceSoapClient.MNBArfolyamServiceSoapClient();
+
+            var currenciesRequestBody = new GetCurrenciesRequestBody();
+
+            var currencyUnitsRequestBody = new GetCurrencyUnitsRequestBody();
+
+            var currencyExchangeRatesRequestBody = new GetCurrentExchangeRatesRequestBody();
+
+            var currencies = client.GetCurrencies(currenciesRequestBody);
+
+            currencyUnitsRequestBody.currencyNames = "HUF,AUD,EUR,USD,IDR,JPY,ITL,GRD";
+
+            var currencyUnits = client.GetCurrencyUnits(currencyUnitsRequestBody);
+
+            var currencyExchangeRates = client.GetCurrentExchangeRates(currencyExchangeRatesRequestBody);
+
+            string currenciesXMLString = XElement.Parse(currencies.GetCurrenciesResult).ToString();    
+
+            Console.WriteLine("currencies: ");
+            Console.WriteLine();
+            Console.WriteLine(currenciesXMLString);
+            Console.WriteLine();
+
+            string currencyUnitsXMLString = XElement.Parse(currencyUnits.GetCurrencyUnitsResult).ToString();
+            Console.WriteLine("currency units : ");
+            Console.WriteLine();
+            Console.WriteLine(currencyUnitsXMLString);
+            Console.WriteLine();
+
+            string exchangeRatesXMLString = XElement.Parse(currencyExchangeRates.GetCurrentExchangeRatesResult).ToString();
+            
+            Console.WriteLine();
+            Console.WriteLine("exchangeRatesXMLString: ");
+            Console.WriteLine();
+            Console.WriteLine(exchangeRatesXMLString);
+
+            Console.ReadLine();
+
+        }
+    }
+}
