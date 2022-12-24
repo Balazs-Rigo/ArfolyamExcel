4af028a9189834ef6feb73751d817e10ab14634a
Balazs Rigo <rigo.balazs86@gmail.com>
2022-12-24 21:55:45 +0100

    Get currencies into list then pass them to properties comma seperated

--- KPMG.Arfolyam/KPMG.Arfolyam/Program.cs
+++ KPMG.Arfolyam/KPMG.Arfolyam/Program.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
 using System.Xml.Linq;
 
 namespace KPMG.Arfolyam
@@ -13,13 +14,24 @@ namespace KPMG.Arfolyam
     {
         static void Main(string[] args)
         {
+           
             MNBArfolyamServiceSoapClient.MNBArfolyamServiceSoapClient client = new MNBArfolyamServiceSoapClient.MNBArfolyamServiceSoapClient();
+           
+            client.Open();
 
             var currenciesRequestBody = new GetCurrenciesRequestBody();
 
             var currencyUnitsRequestBody = new GetCurrencyUnitsRequestBody();
 
-            var currencyExchangeRatesRequestBody = new GetCurrentExchangeRatesRequestBody();
+            var exchangeRatesRequestBody = new GetExchangeRatesRequestBody();
+
+            var currentExchangeRatesRequestBody = new GetCurrentExchangeRatesRequestBody();
+
+            var dateIntervalRequestBody = new GetDateIntervalRequestBody();
+
+            var dateInterval = client.GetDateInterval(dateIntervalRequestBody);
+
+            var intervalDataResult = dateInterval.GetDateIntervalResult;
 
             var currencies = client.GetCurrencies(currenciesRequestBody);
 
@@ -27,7 +39,25 @@ namespace KPMG.Arfolyam
 
             var currencyUnits = client.GetCurrencyUnits(currencyUnitsRequestBody);
 
-            var currencyExchangeRates = client.GetCurrentExchangeRates(currencyExchangeRatesRequestBody);
+            #region get currencies to list
+            XmlDocument xml = new XmlDocument();
+            xml.LoadXml(currencies.GetCurrenciesResult);
+
+            List<string> currencyList = new List<string>();
+            XmlNodeList xnList = xml.SelectNodes("/MNBCurrencies/Currencies/Curr");
+            foreach (XmlNode xn in xnList)
+            {
+                currencyList.Add(xn.InnerText);
+            }
+            #endregion
+
+            exchangeRatesRequestBody.startDate = "2022-10-01";
+            exchangeRatesRequestBody.endDate = "2022-12-02";
+            exchangeRatesRequestBody.currencyNames = string.Join(",",currencyList.Take(11));
+
+            var exchangeRates = client.GetExchangeRates(exchangeRatesRequestBody);
+
+            var currentExchangeRates = client.GetCurrentExchangeRates(currentExchangeRatesRequestBody);
 
             string currenciesXMLString = XElement.Parse(currencies.GetCurrenciesResult).ToString();    
 
@@ -36,21 +66,25 @@ namespace KPMG.Arfolyam
             Console.WriteLine(currenciesXMLString);
             Console.WriteLine();
 
+            
+
             string currencyUnitsXMLString = XElement.Parse(currencyUnits.GetCurrencyUnitsResult).ToString();
             Console.WriteLine("currency units : ");
             Console.WriteLine();
             Console.WriteLine(currencyUnitsXMLString);
             Console.WriteLine();
 
-            string exchangeRatesXMLString = XElement.Parse(currencyExchangeRates.GetCurrentExchangeRatesResult).ToString();
+            string exchangeRatesXMLString = XElement.Parse(exchangeRates.GetExchangeRatesResult).ToString();
             
             Console.WriteLine();
             Console.WriteLine("exchangeRatesXMLString: ");
             Console.WriteLine();
             Console.WriteLine(exchangeRatesXMLString);
 
+            client.Close();
+
             Console.ReadLine();
 
-        }
+        }       
     }
 }
